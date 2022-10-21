97e79bb47e2ac72aa5ed27c34b1732ced1d2258d
Malin Leander <id19mlr@cs.umu.se>
2022-10-21 16:41:21 +0200

    Working dropdown uppdrag

--- Components/QuestList.js
+++ Components/QuestList.js
@@ -1,11 +1,12 @@
 import react from "react";
-import { StyleSheet, Text, View , Image, VirtualizedList} from 'react-native';
+import { StyleSheet, Text, View , Image, TouchableOpacity, VirtualizedList} from 'react-native';
 import { teaGreen, gold, midGreen, darkGreen } from '../Utils/Colors';
 import { useState } from 'react';
 
 export default function QuestList({style}) {
     const [location, setLocation] = useState({X: '63.8225153', Y: '20.2713224', name: 'Döbelns park', description: '', image: ''});
-  
+    const [shouldShow, setshouldShow] = useState(false);
+
     data = [
       {
         id: 1,
@@ -46,47 +47,82 @@ export default function QuestList({style}) {
     const Item = (item) => (
       <View style={style.item}>
         <Text style={style.heading6}>{item.title}</Text>
+        
       </View>
     );
 
+
+
     return (
         
-        <View style={internalStyles.questContainer}>   
+      <View style={shouldShow ? internalStyles.questContainerOpen : internalStyles.questContainerClosed}>   
         <View style={internalStyles.questHeadline}>
-        <Text style={style.heading5white}>Uppdrag</Text>
-        <Image style={internalStyles.icon} source={require('../assets/Arrow.png')}/>
+          <View style={{flexDirection:'row'}}>
+            <Image style={internalStyles.starIcon} source={require('../assets/BsStarFill.png')}/>
+            <Text style={style.heading5white}>Uppdrag</Text>
+          </View>
+          <TouchableOpacity onPress={() => setshouldShow(!shouldShow)}>
+           <Image style={shouldShow ? internalStyles.closeIcon : internalStyles.openIcon} source={require('../assets/Arrow.png')}/>
+          </TouchableOpacity>
         </View>
-        <VirtualizedList showsVerticalScrollIndicator={false}
-        style = {internalStyles.list}
-        data={data}
-        initialNumToRender={4}
-        renderItem={({ item }) => <Item title={item.title} key={item.id} />}
-        keyExtractor={item => item.id}
-        getItemCount={data => data.length}
-        getItem={getItem}
-        />
-        </View>
+        {
+          shouldShow ? ( 
+                        
+            <VirtualizedList showsVerticalScrollIndicator={false}
+              style = {internalStyles.list}
+              data={data}
+              initialNumToRender={4}
+              renderItem={({ item }) => <Item title={item.title} key={item.id} />}
+              keyExtractor={item => item.id}
+              getItemCount={data => data.length}
+              getItem={getItem}
+            />
+                        
+                        
+          ) : null
+        }
+      </View>
+        
     );
 };
 
 const internalStyles = StyleSheet.create({
 
-    questContainer: {
+    questContainerClosed: {
         backgroundColor:darkGreen,
-        borderRadius:10,
-        flex:0.975,
+        borderRadius:30,   
         margin:10,
-        padding:10
+        paddingHorizontal:10, 
+        paddingTop:5
     },
+    questContainerOpen: {
+      backgroundColor:darkGreen,
+      borderRadius:15,   
+      flex:0.975,
+      margin:10,
+      padding:10, 
+  },
     questHeadline: {
         flexDirection:'row', 
         justifyContent: 'space-between', 
         margin:5
     },
-    icon: {
+    openIcon: {
         width: 18,
         height:10,
         margin: 10,
+    },
+    closeIcon: {
+      width: 18,
+      height:10,
+      margin: 10,
+      transform: [{rotate: "180deg"}]
+    },
+    starIcon: {
+      width: 18,
+      height:15,
+      marginTop: 5,
+      marginRight: 8
     },
     item: {
         height: 100,
